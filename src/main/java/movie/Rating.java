96a4189f7303eb13e5a4f64a65dbbf71798486f8
llsrrll96 <58140426+llsrrll96@users.noreply.github.com>
2020-12-05 18:32:48 +0900

    rating , theater

--- src/main/java/movie/Rating.java
+++ src/main/java/movie/Rating.java
@@ -1,14 +1,65 @@
 package movie;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name="RATING")
 public class Rating extends BaseEntity
 {
+	@Id
+	@Column(name = "RATING_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "MOV_NUM")
 	private Long movNum;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "WRITE_TIME")
 	private Date writeTime;		//timestampe
+	@Column(name = "STAR_RATE")
 	private Long starRate;
+	@Column(name = "CONTENT")
 	private String content;
+	@Column(name = "WRITE_ID")
 	private String writeId;
-	
+
+	public Long getMovNum() {
+		return movNum;
+	}
+
+	public void setMovNum(Long movNum) {
+		this.movNum = movNum;
+	}
+
+	public Date getWriteTime() {
+		return writeTime;
+	}
+
+	public void setWriteTime(Date writeTime) {
+		this.writeTime = writeTime;
+	}
+
+	public Long getStarRate() {
+		return starRate;
+	}
+
+	public void setStarRate(Long starRate) {
+		this.starRate = starRate;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWriteId() {
+		return writeId;
+	}
+
+	public void setWriteId(String writeId) {
+		this.writeId = writeId;
+	}
 }
--- src/main/java/movie/Theater.java
+++ src/main/java/movie/Theater.java
@@ -1,13 +1,57 @@
 package movie;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "THEATER")
 public class Theater
 {
+	@Id
+	@Column(name = "THEATER_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "ADDRESS")
 	private String address;
+	@Temporal(TemporalType.DATE)
+	@Column(name = "CLOSED_DATE")
 	private Date closedDate; //date
+	@Column(name = "THE_INCOME")
 	private Long theIncome;
+	@Column(name = "NUM_OF_EMP")
 	private Long numOfEmp;
+
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getClosedDate() {
+		return closedDate;
+	}
+
+	public void setClosedDate(Date closedDate) {
+		this.closedDate = closedDate;
+	}
+
+	public Long getTheIncome() {
+		return theIncome;
+	}
+
+	public void setTheIncome(Long theIncome) {
+		this.theIncome = theIncome;
+	}
+
+	public Long getNumOfEmp() {
+		return numOfEmp;
+	}
+
+	public void setNumOfEmp(Long numOfEmp) {
+		this.numOfEmp = numOfEmp;
+	}
 }
 
