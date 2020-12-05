7de021acad232f66b248d01fc6557e21d6100771
km786 <rjsxo789@naver.com>
2020-12-05 18:08:28 +0900

    .

--- src/main/java/movie/Seat.java
+++ src/main/java/movie/Seat.java
@@ -1,9 +1,17 @@
 package movie;
 
+import javax.persistence.*;
+
+@Entity
 public class Seat
 {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "SEAT_ID")
 	private Long id;
+	@Column(name = "SEAT_NUM")
 	private Long seatNum;
+	@Column(name = "SEAT_SPEICAL_NOTE")
 	private String seatSpecialNote;
+	@Column(name = "SEAT_NAME")
 	private String seatName;
 }
